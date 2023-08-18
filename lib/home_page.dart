266afcfe5d5adddf251df59375495902df9faaa5
Karthik Ponnam <ponnamkarthik3@gmail.com>
2023-08-18 11:38:12 +0530

    initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Live Test"),
+      ),
+      body: const Center(
+        child: Text("Hello World"),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
